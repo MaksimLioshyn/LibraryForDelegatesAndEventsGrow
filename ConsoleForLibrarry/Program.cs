cfd91065e2231ea65247993da12f57ca5ca89711
Maksim Lioshyn <Maksim_Lioshyn@epam.com>
2018-03-30 14:09:33 +0300

    Add first example

---
+++ ConsoleForLibrarry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LibraryForDelegatesAndEventsGrow;
+
+namespace ConsoleForLibrarry
+{
+    public delegate void FirstDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First
+            FirstExampleDelegate firstExampleDelegate = new FirstExampleDelegate();
+            FirstDelegate firstDelegate = new FirstDelegate(firstExampleDelegate.Method);
+            firstDelegate.Invoke();
+            //or
+            firstDelegate();
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LibraryForDelegatesAndEventsGrow/FirstExampleDelegate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryForDelegatesAndEventsGrow
+{
+    public class FirstExampleDelegate
+    {
+        public void Method()
+        {
+            Console.WriteLine("First delegate.");
+        }
+    }
+}
