31073bd88e04cbe47c6b1962edc55a75a15cb0af
Maksim Lioshyn <Maksim_Lioshyn@epam.com>
2018-03-30 15:00:32 +0300

    Add second and fird example

--- ConsoleForLibrarry/Program.cs
+++ ConsoleForLibrarry/Program.cs
@@ -4,11 +4,14 @@ using LibraryForDelegatesAndEventsGrow;
 namespace ConsoleForLibrarry
 {
     public delegate void FirstDelegate();
+    public delegate string SecondDelegate(string name);
+    public delegate void FirdDelegate();
     class Program
     {
         static void Main(string[] args)
         {
-            #region First
+            #region first
+
             FirstExampleDelegate firstExampleDelegate = new FirstExampleDelegate();
             FirstDelegate firstDelegate = new FirstDelegate(firstExampleDelegate.Method);
             firstDelegate.Invoke();
@@ -16,6 +19,80 @@ namespace ConsoleForLibrarry
             firstDelegate();
 
             #endregion
+
+
+            #region second
+
+            SecondExampleDelegate secondExampleDelegate = new SecondExampleDelegate();
+            SecondDelegate secondDelegate = secondExampleDelegate.Method;
+            Console.WriteLine(secondDelegate("Hello World"));
+
+            #endregion
+
+            #region fird
+
+            FirdExampleDelegate firdExampleDelegate = new FirdExampleDelegate();
+
+            FirdDelegate firdDelegate1 = firdExampleDelegate.Method1;
+            FirdDelegate firdDelegate2 = firdExampleDelegate.Method2;
+            FirdDelegate firdDelegate3 = firdExampleDelegate.Method3;
+
+            var firdDelegate = firdDelegate1 + firdDelegate2 + firdDelegate3;
+
+            Console.WriteLine("Enter a number between 1 and 7");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                {
+                    firdDelegate1.Invoke();
+                    break;
+                }
+                case "2":
+                {
+                    firdDelegate2.Invoke();
+                    break;
+                }
+                case "3":
+                {
+                    firdDelegate3.Invoke();
+                    break;
+                }
+                case "4":
+                {
+                    FirdDelegate myDelegate4 = firdDelegate - firdDelegate1;
+                    myDelegate4.Invoke();
+                    break;
+                }
+                case "5":
+                {
+                    FirdDelegate myDelegate5 = firdDelegate - firdDelegate2;
+                    myDelegate5.Invoke();
+                    break;
+                }
+                case "6":
+                {
+                    FirdDelegate myDelegate6 = firdDelegate - firdDelegate3;
+                    myDelegate6.Invoke();
+                    break;
+                }
+                case "7":
+                {
+                    firdDelegate.Invoke();
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exception.");
+                    break;
+                }
+            }
+
+            #endregion
+
+
+
 
             Console.ReadKey();
         }
---
+++ LibraryForDelegatesAndEventsGrow/SecondExampleDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForDelegatesAndEventsGrow
+{
+    public class SecondExampleDelegate
+    {
+        public string Method(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
