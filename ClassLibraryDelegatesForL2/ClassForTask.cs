1ae437e67912f365439315c8e10d066fff3a7186
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-08-23 14:26:50 +0300

    Create first task and tests for it

---
+++ ClassLibraryDelegatesForL2/ClassForTask.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace ClassLibraryDelegatesForL2
+{
+    public class ClassForTask
+    {
+        private int _x1 = 1;
+        private int _x2 = 1;
+
+        public delegate int Fibonacii(int x1, int x2);
+
+        public int NextFicNumb(Fibonacii fibonacii)
+        {
+            int boof = _x2;
+            _x2 = fibonacii(_x1, _x2);
+            _x1 = boof;
+            return _x2;
+        }
+    }
+}
---
+++ ClassLibraryDelegatesForL2Tests/ClassForTaskTests.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibraryDelegatesForL2;
+using NUnit.Framework;
+
+
+namespace ClassLibraryDelegatesForL2Tests
+{
+    [TestFixture()]
+    public class ClassForTaskTests
+    {
+        public int FibonachiiFunction(int x1, int x2) => x1 + x2;
+
+        ClassForTask.Fibonacii _f = null;
+        [TestCase(0, ExpectedResult = 2)]
+        [TestCase(1, ExpectedResult = 3)]
+        [TestCase(2, ExpectedResult = 5)]
+        public int ShowFibonachiiTest(int circle)
+        {
+            _f += FibonachiiFunction;
+            ClassForTask classForTask = new ClassForTask();
+            for (int i = 0; i < circle; i++)
+            {
+                classForTask.NextFicNumb(_f);
+            }
+            return classForTask.NextFicNumb(_f);
+        }
+    }
+}
