93149cdc0c43bc1fb74d7e95c80b9d4d07d07057
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-08-23 17:04:54 +0300

    Add second task for delegates L2

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace WpfApp
 {
@@ -15,6 +18,18 @@ namespace WpfApp
         private void showHideBtn_Click(object sender, RoutedEventArgs e)
         {
             ImageTest.Visibility = !ImageTest.IsVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void increaseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ImageTest.Height = ActualHeight * 1.1;
+            ImageTest.Width = ActualWidth * 1.1;
+        }
+
+        private void DecreaseBtn_OnClickcreaseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ImageTest.Height = ActualHeight * 0.8;
+            ImageTest.Width = ActualWidth * 0.8;
         }
     }
 }
